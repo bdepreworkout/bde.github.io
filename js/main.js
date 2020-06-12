e767418e5392294c0939213da5c3c58bdf431941
alex hay <alexanderhay2020@u.northwestern.edu>
2020-06-12 02:48:12 -0500

    general update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 
 	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
+	    paginationSpeed : 500,
 	    singleItem:true,
 	    autoPlay: 3000,
 	});
@@ -39,15 +39,19 @@ $(document).ready(function(){
 
 
 	// Counter
-
 	$('.counter').counterUp({
         delay: 10,
         time: 1000
     });
 
+	// Slider
+	var slider = document.getElementById("myRange");
+	var output = document.getElementById("demo");
+	output.innerHTML = slider.value; // Display the default slider value
+
+	// Update the current slider value (each time you drag the slider handle)
+	slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
 });
-
-
-
-
