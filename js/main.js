ebe02ac73f4aa317cf79670f01fc724063ed8fa6
alex hay <alexanderhay2020@u.northwestern.edu>
2020-06-12 16:38:24 -0500

    logo redirects to home page, first demo

--- js/main.js
+++ js/main.js
@@ -50,8 +50,6 @@ $(document).ready(function(){
 	output.innerHTML = slider.value; // Display the default slider value
 
 	// Update the current slider value (each time you drag the slider handle)
-	slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+	slider.oninput = function() {output.innerHTML = this.value;}
 
 });
